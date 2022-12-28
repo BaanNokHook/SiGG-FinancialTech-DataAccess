fa6cd0047025694206c0f4a7360bb1407aa73cc2
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:26:05 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IRPConfirmationRepositrory

---
+++ IRPConfirmationRepositrory.cs
@@ -0,0 +1,11 @@
+﻿using GM.Model.Common;
+using GM.Model.PaymentProcess;
+using GM.Model.RPTransaction;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IRPConfirmationRepositrory : IRepository<RPConfirmationModel>
+    {
+        ResultWithModel Get(RPTransModel model);
+    }
+}
