22233339279fa7cfa6e978f5f05ead498dd0e224
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:32:03 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IRPEarlyTerminationRepository

---
+++ IRPEarlyTerminationRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GM.Model.Common;
+using GM.Model.RPTransaction;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IRPEarlyTerminationRepository
+    {
+        ResultWithModel Get(RPTransModel model);
+        ResultWithModel GetDetail(RPTransModel model);
+        ResultWithModel GetColl(RPTransModel model);
+        ResultWithModel Update(RPTransModel model);
+        ResultWithModel UpdateColl(DateTime? terminate_date, RPTransCollateralModel model);
+        ResultWithModel Calculate(RPTransModel model);
+        ResultWithModel Reject(RPTransModel model);
+    }
+}
