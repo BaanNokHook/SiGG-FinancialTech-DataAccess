5caa988691c685f8ddfa28561e0d0a9fad00b705
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:04:06 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IInterfaceMarketPriceRepository

---
+++ IInterfaceMarketPriceRepository.cs
@@ -0,0 +1,13 @@
+﻿using GM.Model.Common;
+using GM.Model.ExternalInterface;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IInterfaceMarketPriceRepository
+    {
+        ResultWithModel Get(InterfaceMarketPriceModel model);
+        ResultWithModel Add(InterfaceMarketPriceDetailModel model);
+        ResultWithModel Remove(InterfaceMarketPriceModel model);
+        ResultWithModel Import(InterfaceMarketPriceModel model);
+    }
+}
