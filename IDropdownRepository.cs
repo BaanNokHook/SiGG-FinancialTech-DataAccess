8920aa8c118591e7e54eafa18742161970ebe973
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 15:18:25 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IDropdownRepository

---
+++ IDropdownRepository.cs
@@ -0,0 +1,9 @@
+﻿using GM.Model.Common;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IDropdownRepository<T> where T : class
+    {
+        ResultWithModel Get(T model);
+    }
+}
