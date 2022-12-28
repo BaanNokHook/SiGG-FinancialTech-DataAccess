755b11211fb48cc56ab7144a9e5ba76a9205f612
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 15:20:28 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IExportAmendCancelRepository

---
+++ IExportAmendCancelRepository.cs
@@ -0,0 +1,12 @@
+﻿using GM.Model.Common;
+using GM.Model.ExternalInterface;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IExportAmendCancelRepository
+    {
+        ResultWithModel GetAmendCancelDaily(ExportAmendCancelDailyMailModel model);
+
+        ResultWithModel GetAmendCancelMonthly(ExportAmendCancelMonthlyMailModel model);
+    }
+}
