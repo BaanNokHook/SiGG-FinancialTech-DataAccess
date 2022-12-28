743ca5c28fb6cfa0aba88cb569f8a98ebe030fd3
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:36:07 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IRPReleaseMessageRepository

---
+++ IRPReleaseMessageRepository.cs
@@ -0,0 +1,28 @@
+﻿using GM.Model.Common;
+using GM.Model.PaymentProcess;
+using GM.Model.RPTransaction;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IRPReleaseMessageRepository
+    {
+        ResultWithModel Get(RPTransModel model);
+        ResultWithModel GetNetSettle(RPTransModel model);
+        ResultWithModel GetList(RPTransModel model);
+        ResultWithModel GetListPTI(RPTransModel model);
+        ResultWithModel GetListNetSettle(RPTransModel model);
+        ResultWithModel GetConfirm(RPTransModel model);
+        ResultWithModel GetAmendConfirm(RPTransModel model);
+        ResultWithModel GetConfirmPRP(RPTransModel model);
+        ResultWithModel Add(RPReleaseMessageModel model);
+        ResultWithModel MtList(RPTransModel model);
+        ResultWithModel RPCouponList(RPCouponModel model);
+        ResultWithModel CallMarginList(RPCallMarginModel model);
+        ResultWithModel CallMarginGet(RPCallMarginModel model);
+        ResultWithModel CallMargin(RPCallMarginModel model);
+        ResultWithModel CheckPayment(RPReleaseMsgCheckPaymentModel model);
+        ResultWithModel DetailCoupon(RPCouponModel model);
+        ResultWithModel InterestMarginList(RPCallMarginModel model);
+        ResultWithModel InterestMargin(RPCallMarginModel model);
+    }
+}
