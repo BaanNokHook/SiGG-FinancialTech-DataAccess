1a15864d507999477c115f4088c6afa42cdb98bf
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:22:09 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IRPApproveRepository

---
+++ IRPApproveRepository.cs
@@ -0,0 +1,13 @@
+﻿using GM.Model.Common;
+using GM.Model.RPTransaction;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IRPApproveRepository
+    {
+        ResultWithModel Get(RPTransModel model);
+        ResultWithModel GetColl(RPTransModel model);
+        ResultWithModel Update(RPTransModel model);
+        ResultWithModel UpdateColl(RPTransCollateralModel model);
+    }
+}
