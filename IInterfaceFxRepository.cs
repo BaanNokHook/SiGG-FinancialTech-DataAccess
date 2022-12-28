3da7884799887e227635053aab742fda4f2f20a6
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:02:04 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IInterfaceFxRepository

---
+++ IInterfaceFxRepository.cs
@@ -0,0 +1,14 @@
+﻿using GM.Model.Common;
+using GM.Model.ExternalInterface;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IInterfaceFxRepository
+    {
+        ResultWithModel GetTransaction(InterfaceFxReconcileSftpModel model);
+
+        ResultWithModel GetPosition(InterfaceFxReconcileSftpModel model);
+
+        ResultWithModel GetPostingEvent(InterfaceFxReconcileSftpModel model);
+    }
+}
