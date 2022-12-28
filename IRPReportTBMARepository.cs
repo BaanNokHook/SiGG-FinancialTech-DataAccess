cad5764298bd7edcbb652584e6c64608be4755db
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:38:08 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IRPReportTBMARepository

---
+++ IRPReportTBMARepository.cs
@@ -0,0 +1,11 @@
+﻿using GM.Model.Common;
+using GM.Model.RPTransaction;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IRPReportTBMARepository
+    {
+        ResultWithModel GetList(RPTransModel model);
+        ResultWithModel GetDetail(RPTransModel model, string exportType);
+    }
+}
