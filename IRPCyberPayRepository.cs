5e34e6060f7f02ac4637cf2e3f48d5c82735adb2
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:30:05 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IRPCyberPayRepository

---
+++ IRPCyberPayRepository.cs
@@ -0,0 +1,10 @@
+﻿using GM.Model.Common;
+using GM.Model.PaymentProcess;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IRPCyberPayRepository
+    {
+        ResultWithModel Add(string transDate, string cur);
+    }
+}
