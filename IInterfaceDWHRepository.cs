1d5cf54f6cef2df9bb75e7d8a7093839d2757bfb
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 15:50:17 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IInterfaceDWHRepository

---
+++ IInterfaceDWHRepository.cs
@@ -0,0 +1,13 @@
+﻿using GM.Model.Common;
+using GM.Model.ExternalInterface;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IInterfaceDWHRepository
+    {
+        ResultWithModel Add(InterfaceDwhSftpModel model);
+        ResultWithModel GetList(InterfaceDwhSftpModel model);
+        ResultWithModel GetSftp(InterfaceDwhSftpModel model);
+
+    }
+}
