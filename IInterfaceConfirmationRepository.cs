0e5316965d6ff6ca32c5f8c45f5c955ffa0658d5
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 15:45:24 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IInterfaceConfirmationRepository

---
+++ IInterfaceConfirmationRepository.cs
@@ -0,0 +1,15 @@
+﻿using GM.Model.Common;
+using GM.Model.ExternalInterface;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IInterfaceConfirmationRepository
+    {
+        ResultWithModel GetList(InterfaceCCMSearch model);
+
+        ResultWithModel AddLog(string ref_id, string trans_no, string trans_type_code, string create_by);
+
+        ResultWithModel GetSignName(string trans_no, string create_by);
+
+    }
+}
