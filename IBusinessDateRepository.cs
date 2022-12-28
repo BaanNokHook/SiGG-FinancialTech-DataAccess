67156ff298e1d4336032f922ab5f5e1c3434d832
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 15:15:58 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IBusinessDateRepository

---
+++ IBusinessDateRepository.cs
@@ -0,0 +1,11 @@
+﻿using GM.Model.Common;
+using GM.Model.Static;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IBusinessDateRepository
+    {
+        ResultWithModel GetBusinessDate(BusinessDateModel model);
+        ResultWithModel GetBusinessDateOrSystemDate(BusinessDateModel model);
+    }
+}
