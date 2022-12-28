6afb6f2136a7ef2090e02301d7b2952c829a90d4
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 17:15:05 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-ITestConnectRepository

---
+++ ITestConnectRepository.cs
@@ -0,0 +1,10 @@
+﻿using GM.Model.Common;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface ITestConnectRepository<TestConnectModel>
+    {
+        ResultWithModel GetTestDatabaseInfo(TestConnectModel model);
+        ResultWithModel GetExternalService(TestConnectModel model);
+    }
+}
