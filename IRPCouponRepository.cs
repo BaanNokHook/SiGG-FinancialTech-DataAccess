dfa32708d1aaa370402817e8cb8e38d4982cf355
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:28:13 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IRPCouponRepository

---
+++ IRPCouponRepository.cs
@@ -0,0 +1,12 @@
+﻿using GM.Model.Common;
+using GM.Model.PaymentProcess;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IRPCouponRepository
+    {
+        ResultWithModel Get(RPCouponModel model);
+        ResultWithModel GetList(RPCouponModel model);
+        ResultWithModel GetDetail(RPCouponModel model);
+    }
+}
