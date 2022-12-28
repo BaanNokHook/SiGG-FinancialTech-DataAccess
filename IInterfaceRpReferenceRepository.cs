f381c00dc357d6b5281482b4c826e4e76ac915f9
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:12:04 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IInterfaceRpReferenceRepository

---
+++ IInterfaceRpReferenceRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GM.Model.Common;
+using GM.Model.InterfaceRpReference;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IInterfaceRpReferenceRepository
+    {
+        ResultWithModel Add(ReqRpReferenceList model);
+        ResultWithModel Remove(DateTime asofDate, string settlementDay);
+        ResultWithModel Import(DateTime asofDate, string settlementDay);
+    }
+}
