23edcdab46da20a27138684742f2fb1c3ab8a652
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:24:14 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IRPCallMarginRepository

---
+++ IRPCallMarginRepository.cs
@@ -0,0 +1,18 @@
+﻿using GM.Model.Common;
+using GM.Model.PaymentProcess;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IRPCallMarginRepository
+    {
+        ResultWithModel Get(string callDateFrom, string callDateTo, string ctpyID, string cur);
+        ResultWithModel GetDetail(string asOfDate, int ctpyID);
+        ResultWithModel GetDetailBRP(string asOfDate, int ctpyID, string cur);
+        ResultWithModel GetList(RPCallMarginModel model);
+        ResultWithModel Add(string transDate, string cur, decimal? holiday_int_rate);
+        ResultWithModel AddAdjustPRP(RPCallMarginPRPModel model);
+        ResultWithModel AddAdjustBRP(RPCallMarginBRPModel model);
+        ResultWithModel ActionInterestRate(string transDate, string cur, decimal interestRate);
+        ResultWithModel CheckMarginIntRate(RPCallMarginModel model);
+    }
+}
