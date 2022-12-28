806996e6feb40668f3f8db100dac0a1a6719a3a8
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:18:09 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IRepository

---
+++ IRepository.cs
@@ -0,0 +1,25 @@
+﻿using GM.Model.Common;
+using System.Collections.Generic;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IRepository<T> where T : class
+    {
+        ResultWithModel Get(T model);
+
+        ResultWithModel Find(T model);
+
+        ResultWithModel Add(T model);
+
+        ResultWithModel AddList(List<T> models);
+
+        ResultWithModel Update(T model);
+
+        ResultWithModel UpdateList(List<T> models);
+
+        ResultWithModel Remove(T model);
+
+        // exception it shouldn't be here just for conveninace
+       
+    }
+}
