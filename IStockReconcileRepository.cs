50b743bc58f168bca79c0e9c1250a568ec1ca043
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 17:10:15 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IStockReconcileRepository

---
+++ IStockReconcileRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GM.Model.Common;
+using GM.Model.StockReconcile;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IStockReconcileRepository
+    {
+        ResultWithModel AddTsd(StockReconcileImportModel model);
+        ResultWithModel Import(DateTime asofDate, string import_id);
+        ResultWithModel CheckRemark(DateTime asofDate);
+        ResultWithModel Get(StockReconcileModel model);
+        ResultWithModel Save(StockReconcileModel model);
+    }
+}
