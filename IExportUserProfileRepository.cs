354d511177db4cb9dd7010d04c88cc8ed99448e7
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 15:25:12 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IExportUserProfileRepository

---
+++ IExportUserProfileRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GM.Model.Common;
+using GM.Model.ExternalInterface;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IExportUserProfileRepository
+    {
+        ResultWithModel GetUserProfileMonthly(ExportUserProfileMonthlyMailModel model);
+    }
+}
