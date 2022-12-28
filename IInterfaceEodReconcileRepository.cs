fa4aaf4cee022b890e6d7faf3326015a5c82149c
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 15:55:48 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IInterfaceEodReconcileRepository

---
+++ IInterfaceEodReconcileRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GM.Model.Common;
+using GM.Model.InterfaceEodReconcile;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IInterfaceEodReconcileRepository
+    {
+        ResultWithModel AddPTI(DateTime asofDate, ReqEodReconcilePti model);
+        ResultWithModel RemovePTI(DateTime asofDate, string recordedBy);
+        ResultWithModel AddBahtnet(DateTime asofDate, ReqEodReconcileBahtnet model);
+        ResultWithModel RemoveBahtnet(DateTime asofDate, string recordedBy);
+
+        ResultWithModel Get(DateTime asofDate);
+        ResultWithModel Add(RPEodReconcileModel model);
+    }
+}
