c3523109d9191753cb2df0096a017713fa9416a2
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:14:09 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-ILoginRepository

---
+++ ILoginRepository.cs
@@ -0,0 +1,12 @@
+﻿using GM.Model.Common;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface ILoginRepository<LoginModel>
+    {
+        ResultWithModel Authenticate(LoginModel model);
+        ResultWithModel Online(LoginModel model);
+        ResultWithModel Offline(LoginModel model);
+        ResultWithModel IsStillOnline(LoginModel model);
+    }
+}
