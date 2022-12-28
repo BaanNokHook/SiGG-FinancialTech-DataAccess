3639e052b61f5be0655c2912a7a16b19cb708f83
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:06:06 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IInterfaceMarketPriceTbmaRepository

---
+++ IInterfaceMarketPriceTbmaRepository.cs
@@ -0,0 +1,16 @@
+﻿using GM.Model.Common;
+using GM.Model.ExternalInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IInterfaceMarketPriceTbmaRepository
+    {
+        ResultWithModel Add(InterfaceMarketPriceTbmaDetailModel model);
+        ResultWithModel Remove(InterfaceMarketPriceTbmaModel model);
+        ResultWithModel Import(InterfaceMarketPriceTbmaModel model);
+        ResultWithModel Check(InterfaceMarketPriceTbmaModel model);
+    }
+}
