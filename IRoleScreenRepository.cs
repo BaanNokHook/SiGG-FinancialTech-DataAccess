f787432ef7da1a78f70f520cd13490c7c6add10e
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:20:06 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IRoleScreenRepository

---
+++ IRoleScreenRepository.cs
@@ -0,0 +1,10 @@
+﻿using GM.Model.Common;
+using GM.Model.UserAndScreen;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IRoleScreenRepository : IRepository<RoleAndScreenModel>
+    {
+        ResultWithModel ScreenMenu(string username);
+    }
+}
