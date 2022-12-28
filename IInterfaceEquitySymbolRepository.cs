115786a98dba20565e27552633be8a7c2664c199
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:00:05 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IInterfaceEquitySymbolRepository

---
+++ IInterfaceEquitySymbolRepository.cs
@@ -0,0 +1,15 @@
+﻿using GM.Model.Common;
+using GM.Model.ExternalInterface.InterfaceEquitySymbol;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IInterfaceEquitySymbolRepository
+    {
+        ResultWithModel Add(ReqEquitySymbolList model);
+        ResultWithModel Remove(ReqEquitySymbol model);
+        ResultWithModel Import(ReqEquitySymbol model);
+    }
+}
