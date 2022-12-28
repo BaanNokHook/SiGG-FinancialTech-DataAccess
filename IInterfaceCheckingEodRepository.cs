9953b27be7dc59d1546aa9c60533f4f1782b764d
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 15:40:12 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IInterfaceCheckingEodRepository

---
+++ IInterfaceCheckingEodRepository.cs
@@ -0,0 +1,10 @@
+﻿using GM.Model.Common;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IInterfaceCheckingEodRepository
+    {
+        ResultWithModel Get();
+        ResultWithModel Update(string taskName);
+    }
+}
