1a25954d139bb9fead4e9eeddf763885f05fcd85
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:10:07 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IInterfaceNavPriceRepository

---
+++ IInterfaceNavPriceRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GM.Model.Common;
+using GM.Model.InterfaceNavPrice;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IInterfaceNavPriceRepository
+    {
+        ResultWithModel Add(ReqNavPriceList model);
+        ResultWithModel Remove(ReqNavPrice model);
+        ResultWithModel Import(ReqNavPrice model);
+    }
+}
