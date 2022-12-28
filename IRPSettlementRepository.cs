5d4dc86861a04c9129988830bad7c5c191fb210f
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 16:40:14 +0700

    SiGG-FinancialTech-DataAccess-Quater-Infrastructure-IRPSettlementRepository

---
+++ IRPSettlementRepository.cs
@@ -0,0 +1,12 @@
+﻿using GM.Model.Common;
+using GM.Model.RPTransaction;
+
+namespace GM.DataAccess.Infrastructure
+{
+    public interface IRPSettlementRepository
+    {
+        ResultWithModel Get(RPTransModel model);
+        ResultWithModel GetColl(RPTransModel model);
+        ResultWithModel Update(RPTransModel model);
+    }
+}
