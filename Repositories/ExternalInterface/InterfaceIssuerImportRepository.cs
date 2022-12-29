e9102b51225a9f51ad867a5aa2bc2a86c86cf49f
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-29 19:42:12 +0700

    SiGG-FinancialTech-DataAccess-Quater-Repositories-ExternalInterface-InterfaceIssuerImportRepository

---
+++ Repositories/ExternalInterface/InterfaceIssuerImportRepository.cs
@@ -0,0 +1,57 @@
+﻿using GM.DataAccess.Infrastructure;
+using GM.DataAccess.UnitOfWork;
+using GM.Model.Common;
+using GM.Model.InterfaceIssuer;
+using System;
+using System.Collections.Generic;
+
+namespace GM.DataAccess.Repositories.ExternalInterface
+{
+    public class InterfaceIssuerImportRepository : IRepository<reqIssuer>
+    {
+        private readonly IUnitOfWork _uow;
+        public InterfaceIssuerImportRepository(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+        public ResultWithModel Add(reqIssuer model)
+        {
+            BaseParameterModel parameter = new BaseParameterModel();
+            parameter.ProcedureName = "GM_Issuer_820002_Import_Proc";
+            parameter.Parameters.Add(new Field { Name = "ref_code", Value = model.RefCode });
+            parameter.Parameters.Add(new Field { Name = "recorded_by", Value = model.Function });
+            parameter.ResultModelNames.Add("IssuerResultModel");
+            return _uow.ExecNonQueryProc(parameter);
+        }
+
+        public ResultWithModel AddList(List<reqIssuer> models)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResultWithModel Find(reqIssuer model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResultWithModel Get(reqIssuer model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResultWithModel Remove(reqIssuer model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResultWithModel Update(reqIssuer model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResultWithModel UpdateList(List<reqIssuer> models)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
