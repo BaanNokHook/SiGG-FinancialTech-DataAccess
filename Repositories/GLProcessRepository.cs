4a4ee3902880bfd04cc41ce4c268424ee5d6b95c
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-30 18:54:54 +0700

    SiGG-FinancialTech-DataAccess-Quater-Repositories-GLProcessRepository

---
+++ Repositories/GLProcessRepository.cs
@@ -0,0 +1,19 @@
+﻿using GM.DataAccess.Infrastructure;
+using GM.DataAccess.Repositories.GLProcess;
+using GM.DataAccess.UnitOfWork;
+using GM.Model.GLProcess;
+
+namespace GM.DataAccess.Repositories
+{
+    public class GLProcessRepository
+    {
+        public IRepository<GLAccountCodeModel> GLAccountCode { get; }
+        public IRepository<GLAdjustModel> GLAdjust { get; }
+
+        public GLProcessRepository(IUnitOfWork uow)
+        {
+            GLAccountCode = new GLAccountCodeRepository(uow);
+            GLAdjust = new GLAdjustRepository(uow);
+        }
+    }
+}
