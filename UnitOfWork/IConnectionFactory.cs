57dfabb1214d84a0126e405b3635fcf1527538e6
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-30 19:03:52 +0700

    SiGG-FinancialTech-DataAccess-Quater-UnitOfWork-IConnectionFactory

---
+++ UnitOfWork/IConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace GM.DataAccess.UnitOfWork
+{
+    public interface IConnectionFactory : IDisposable
+    {
+        IDbConnection Create();
+    }
+}
