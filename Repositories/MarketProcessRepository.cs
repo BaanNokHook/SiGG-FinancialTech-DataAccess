8d2ae24b827b8ce7b305f6561a2914cf631c76ee
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-30 18:55:09 +0700

    SiGG-FinancialTech-DataAccess-Quater-Repositories-MarketProcessRepository

---
+++ Repositories/MarketProcessRepository.cs
@@ -0,0 +1,21 @@
+﻿using GM.DataAccess.Infrastructure;
+using GM.DataAccess.Repositories.MarketProcess;
+using GM.DataAccess.UnitOfWork;
+using GM.Model.MarketProcess;
+
+namespace GM.DataAccess.Repositories
+{
+    public class MarketProcessRepository
+    {
+        public IRepository<ExchangeRateModel> ExchangeRate { get; }
+        public IRepository<FloatingIndexModel> FloatingIndex { get; }
+        public IRepository<RPReferenceModel> RPReference { get; }
+
+        public MarketProcessRepository(IUnitOfWork uow)
+        {
+            ExchangeRate = new ExchangeRateRepository(uow);
+            FloatingIndex = new FloatingIndexRepository(uow);
+            RPReference = new RPReferenceRepository(uow);
+        }
+    }
+}
