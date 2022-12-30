e5ea6af8d66c15ea0e725682cf8e6d3d58a37584
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-30 18:55:23 +0700

    SiGG-FinancialTech-DataAccess-Quater-Repositories-PaymentProcessRepository

---
+++ Repositories/PaymentProcessRepository.cs
@@ -0,0 +1,41 @@
+﻿using GM.DataAccess.Infrastructure;
+using GM.DataAccess.Repositories.PaymentProcess;
+using GM.DataAccess.UnitOfWork;
+using GM.Model.PaymentProcess;
+using GM.Model.RPTransaction;
+
+namespace GM.DataAccess.Repositories
+{
+    public class PaymentProcessRepository
+    {
+        public IRPConfirmationRepositrory RPConfirmation { get; }
+        public IRepository<RPCouponModel> RPTransCoupon { get; }
+        public IRepository<RPTransModel> ConfirmBilateral { get; }
+        public IRepository<RPTransModel> ConfirmBilateralEarlyTerminate { get; }
+        public IRepository<RPTransModel> ConfirmAmendDeal { get; }
+        public IRepository<RPCouponDetailModel> RPTransCouponDetail { get; }
+        public IRPCouponRepository RPCoupon { get; }
+        public IRepository<RPMarginInterestModel> RPMarginInterest { get; }
+        public IRPReleaseMessageRepository RPReleaseMessage { get; }
+        public IRPCallMarginRepository RPCallMargin { get; }
+        public IRepository<RPMarginInterestFCYModel> RPMarginInterestFCY { get; }
+        public IRepository<RPReleaseCyberPayModel> RPReleaseCyberPay { get; }
+
+
+        public PaymentProcessRepository(IUnitOfWork uow)
+        {
+            RPConfirmation = new RPConfirmationRepositrory(uow);
+            RPTransCoupon = new RPTransCouponRepository(uow);
+            RPTransCouponDetail = new RPTransCouponDetailRepository(uow);
+            RPCoupon = new RPCouponRepository(uow);
+            RPMarginInterest = new RPMarginInterestRepository(uow);
+            RPMarginInterestFCY = new RPMarginInterestFCYRepository(uow);
+            RPReleaseMessage = new RPReleaseMessageRepository(uow);
+            RPCallMargin = new RPCallMarginRepository(uow);
+            ConfirmBilateral = new RPConfirmationBilateralRepositrory(uow);
+            ConfirmBilateralEarlyTerminate = new RPConfirmationEarlyRepositrory(uow);
+            ConfirmAmendDeal = new RPConfirmationAmendDealRepositrory(uow);
+            RPReleaseCyberPay = new RPReleaseCyberPayRepositrory(uow);
+        }
+    }
+}
